f2fb02369c0519298910d9ae5c58cf50901f3f68
Vijay <rathinavelsubramani56@gmail.com>
2019-10-14 22:50:25 +0530

    initial

--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -54,7 +54,18 @@ class UsersController extends AppController
 	
 	public function account()
 	{
-		//
+		$user = $this->Users->newEntity();
+        if ($this->request->is('post')) {
+            $user = $this->Users->patchEntity($user, $this->request->data);
+            if ($this->Users->save($user)) {
+                $this->Flash->success(__('The user has been saved.'));
+                return $this->redirect(['action' => 'index']);
+            } else {
+                $this->Flash->error(__('The user could not be saved. Please, try again.'));
+            }
+        }
+        $this->set(compact('user'));
+        $this->set('_serialize', ['user']);
 	}
 
     /**
